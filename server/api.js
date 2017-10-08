0375482b42c846612b33b4cfb104fd13e6eaa08f
Akhil Soman <akhil.soman@ge.com>
2017-10-08 21:03:39 +0530

    Adding Emotion API

--- server/api.js
+++ server/api.js
@@ -1,18 +1,37 @@
 // api logic
-const requesthttp = require('request');
+const makerequest = require('request');
 const config = require('./config');
 const _ = require('lodash');
+const fs = require('fs');
+const path = require('path')
+
 const options = {
-  'Content-Type': 'application/json',
-  'method': 'GET'
+  'method': 'POST',
+  'headers': {
+    'Content-Type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key' : config['key1']
+  },
+  'json': false,
 };
-
-
+const getEmotionIndex = function(request,response){
+  let option = _.merge(options,{
+    'uri' : config["api-url"]+"/recognize",
+    'body': fs.readFileSync(`uploads/${request.file.filename}`)
+  });
+  let req = makerequest(option, function(err, res, body) {
+    if(res.statusCode !== 200) {
+      errorHandler(res,response);
+    }else{
+      body = JSON.parse(body)
+      response.json(body)
+    }
+  });
+}
 const errorHandler = function(res,response){
     response.status(res.statusCode);
     response.json(res.statusMessage || 'Something went wrong, Please try again after some time');
 }
 
 module.exports = {
-
+  getEmotionIndex: getEmotionIndex
 }
--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const express = require('express');
 const path    = require('path');
 const compression = require('compression')
 const api    = require('./api');
 const bodyParser = require('body-parser');
+const multer  = require('multer');
+
+//Multer extension
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().getTime() + path.extname(file.originalname)) //Appending extension
+  }
+})
+
+const uplaod = multer({ /*dest: 'uploads/'*/ storage: storage});
 
 
 const port = (process.argv.slice(2).length > 0) ? parseInt(process.argv.slice(2)) : 5000 ;
@@ -16,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //Api routing
-
+app.post('/api/uploadImage', uplaod.single('emoteeinp'), api.getEmotionIndex);
 
 //starting server
 const server = app.listen(port, function () {
