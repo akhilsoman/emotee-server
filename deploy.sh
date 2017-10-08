c07057f961a51f955d4cd90427ae093b747adb53
akhilsoman <akhilsoman@gmail.com>
2017-10-08 23:47:56 +0530

    Updating deployment script

--- deploy.sh
+++ deploy.sh
@@ -118,4 +118,13 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
 fi
 
 ##################################################################################################################################
+# Post deployment stub
+if [[ -n "$POST_DEPLOYMENT_ACTION" ]]; then
+  POST_DEPLOYMENT_ACTION=${POST_DEPLOYMENT_ACTION//\"}
+  cd "${POST_DEPLOYMENT_ACTION_DIR%\\*}"
+  "$POST_DEPLOYMENT_ACTION"
+  exitWithMessageOnError "post deployment action failed"
+fi
+##################################################################################################################################
+
 echo "Finished successfully."
